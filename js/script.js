037199b4d6b0b9e7a8473488d44b298f88f5d6ac
Peter Matthews <pjdmatthews7@gmail.com>
2017-11-26 06:53:44 -0500

    Name the cats

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
+    var cat1 = "Bob";
+    var cat2 = "Bill";
+
+    $("#name1").text(cat1);
+    $("#name2").text(cat2);
+
     var clicky1 = 0;
+    var clicky2 = 0;
+
     $("#target1").click(function() {
         clicky1 += 1;
         $("#counter1").text(clicky1);
     });
-    var clicky2 = 0;
+
     $("#target2").click(function() {
         clicky2 += 1;
         $("#counter2").text(clicky2);
