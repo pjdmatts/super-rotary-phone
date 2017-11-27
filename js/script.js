1107e10fdf7ef2e297b2f9c9b905423f19be8d8b
Peter Matthews <pjdmatthews7@gmail.com>
2017-11-27 06:55:50 -0500

    List o cats with event listener

--- js/script.js
+++ js/script.js
@@ -4,14 +4,41 @@ $(document).ready(function() {
     var $nameElem = $('#catName');
     var $targetElem = $('#target');
     var $countElem = $('#catCounter');
-    var nums = [1,2,3];
-
-    for (var i = 0; i<nums.length; i++) {
-        var num = nums[i];
-        $listElem.append('<li>'+num+'</li>');
-    };
+    var nums = [1, 2, 3];
 
     // Cat stuff
+
+    var cats = [{
+        name: "Bob",
+        image: "https://loremflickr.com/320/240"
+    }, {
+        name: "Arthur",
+        image: "https://loremflickr.com/320/240"
+    }, {
+        name: "Humphrey",
+        image: "https://loremflickr.com/320/240"
+    }, {
+        name: "Whiskey",
+        image: "https://loremflickr.com/320/240"
+    }, {
+        name: "Tango",
+        image: "https://loremflickr.com/320/240"
+    }, {
+        name: "Foxtrot",
+        image: "https://loremflickr.com/320/240"
+    }];
+
+    for (var i = 0; i < cats.length; i++) {
+        var cat = cats[i];
+        var $elem = $('<li>' + cat.name + '</li>');
+        $elem.click((function(catCopy) {
+            return function() {
+                alert(catCopy.name);
+            };
+        })(cat));
+        $listElem.append($elem);
+    }
+
     var cat = "Bob";
 
     $nameElem.text(cat);
