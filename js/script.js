3cf4fb17cabec201b53a4d0ddaa4308471bbe90b
Peter Matthews <pjdmatthews7@gmail.com>
2017-11-28 10:49:48 -0500

    Added functionality to show each cat on a click

--- js/script.js
+++ js/script.js
@@ -4,50 +4,47 @@ $(document).ready(function() {
     var $nameElem = $('#catName');
     var $targetElem = $('#target');
     var $countElem = $('#catCounter');
+    var $imageElem = $('#catPic');
     var nums = [1, 2, 3];
 
     // Cat stuff
 
     var cats = [{
         name: "Bob",
-        image: "https://loremflickr.com/320/240"
+        picture: "https://loremflickr.com/320/240/cat"
     }, {
         name: "Arthur",
-        image: "https://loremflickr.com/320/240"
+        picture: "https://loremflickr.com/320/240/dog"
     }, {
         name: "Humphrey",
-        image: "https://loremflickr.com/320/240"
+        picture: "https://loremflickr.com/320/240/horse"
     }, {
         name: "Whiskey",
-        image: "https://loremflickr.com/320/240"
+        picture: "https://loremflickr.com/320/240/elephant"
     }, {
         name: "Tango",
-        image: "https://loremflickr.com/320/240"
+        picture: "https://loremflickr.com/320/240/fish"
     }, {
         name: "Foxtrot",
-        image: "https://loremflickr.com/320/240"
+        picture: "https://loremflickr.com/320/240/cow"
     }];
+
+
 
     for (var i = 0; i < cats.length; i++) {
         var cat = cats[i];
         var $elem = $('<li>' + cat.name + '</li>');
         $elem.click((function(catCopy) {
             return function() {
-                alert(catCopy.name);
+                $nameElem.text(catCopy.name);
+                $imageElem.attr("src", catCopy.picture);
+                var clicky = 0;
+                $targetElem.click(function() {
+                    clicky += 1;
+                    $countElem.text(clicky);
+                });
             };
         })(cat));
         $listElem.append($elem);
     }
-
-    var cat = "Bob";
-
-    $nameElem.text(cat);
-
-    var clicky = 0;
-
-    $targetElem.click(function() {
-        clicky += 1;
-        $countElem.text(clicky);
-    });
-
 });
