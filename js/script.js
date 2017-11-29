022a8b656cd36d362dd27916b07ec6867c714ab9
Peter Matthews <pjdmatthews7@gmail.com>
2017-11-29 15:09:45 -0500

    Finish MVO Funtionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $(function() {
-
     var model = {
-        currentCat: 0,
+        currentCat: null,
         cats: [{
             name: "Bob",
             catCount: 0,
@@ -9,7 +8,7 @@ $(function() {
         }, {
             name: "Arthur",
             catCount: 0,
-            picture: "resources/grey.jpg"
+            picture: "resources/grey.jpeg"
         }, {
             name: "Humphrey",
             catCount: 0,
@@ -31,12 +30,13 @@ $(function() {
             return model.cats;
         },
         getCat: function() {
-            return model.cats[model.currentCat];
+            return model.currentCat;
         }
     };
 
     var octopus = {
         init: function() {
+            model.currentCat = model.cats[0]
             viewList.init();
             viewWindow.init();
         },
@@ -45,37 +45,56 @@ $(function() {
         },
         getCat: function() {
             return model.getCat();
+        },
+        setCat: function(catIn) {
+            model.currentCat = catIn;
+        },
+        incrementCounter: function() {
+            model.currentCat.catCount++;
+            viewWindow.render();
         }
     };
 
     var viewList = {
         init: function() {
-            viewList.render();
+            this.render();
         },
         render: function() {
-            var $listElem = $('#cat-list');
-            var $elem = ''
+            $listElem = $('#cat-list');
+            var $elem = '';
             octopus.getCats().forEach(function(cat) {
-                // var cat = cats[i];
-                $elem += '<li>' + cat.name + '</li>'
+                $elem = $('<li>' + cat.name + '</li>');
+                $elem.click((function(catCopy) {
+                    return function() {
+                        octopus.setCat(catCopy);
+                        viewWindow.render();
+                    };
+                })(cat));
+                $listElem.append($elem);
             });
-            $listElem.append($elem);
+
+
         }
     };
 
     var viewWindow = {
         init: function() {
-            var $nameElem = $('#catName');
-            var $targetElem = $('#target');
-            var $countElem = $('#catCounter');
-            var $imageElem = $('#catPic');
-            viewWindow.render();
+            this.$nameElem = $('#catName');
+            this.$targetElem = $('#target');
+            this.$countElem = $('#catCounter');
+            this.$imageElem = $('#catPic');
+            this.$targetElem.click(function() {
+                octopus.incrementCounter();
+            });
+            this.render();
         },
         render: function() {
-            
+            var theCat = octopus.getCat();
+            this.$nameElem.text(theCat.name);
+            this.$imageElem.attr("src", theCat.picture);
+            this.$countElem.text(theCat.catCount);
         }
     }
-
     octopus.init();
 });
 // List stuff
